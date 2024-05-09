ad3e5942194a3389398cd274919ceb5e14353ab8
Henry Gasparotto <126111750+henrypedroso@users.noreply.github.com>
2024-05-09 07:31:16 -0300

    ssssssssss

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
 const botoes = document.querySelectorAll(".botao");
-console.log(botoes);
+
+for(let i = 0; i <botoes.length; i++) {
+    botoes[i].onclik = function(){
+        for (let j=0; j<botoes.length; j++){
+            botoes[j].classList.remove("ativo");
+        }
+    }
+}
