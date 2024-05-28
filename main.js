f13a7f689a4cb1e8bd5d8c7abde6b99246c5ba7c
Henry Gasparotto <126111750+henrypedroso@users.noreply.github.com>
2024-05-28 18:05:54 -0300

    ssssss

--- main.js
+++ main.js
@@ -25,6 +25,7 @@ function calculaTempo(tempoObjetivo){
     let tempoFinal = tempoObjetivo - tempoAtual;
     let segundos = Math.floor(tempoFinal/1000);
     let minutos = Math.floor(segundos/60);
-    let horass = Math.floor(minutos/60);
+    let horas = Math.floor(minutos/60);
     return horas;
+
 }
